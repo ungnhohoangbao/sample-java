8589b430c3f05aa368553ed36ca040cf688dec92
Ung Nho Hoang Bao <bao.ung-hoang@internal.banvien.com>
2024-12-04 10:09:23 +0700

    init

---
+++ src/main/java/com/hb/example/sample_java/SampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hb.example.sample_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hb/example/sample_java/SampleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.example.sample_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
